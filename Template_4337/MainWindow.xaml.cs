a795bdda2c1703e736fedf03af4d2615636037c1
tanya <tanyasosnovskaya145@gmail.com>
2023-04-24 12:28:01 +0300

    Sosnovskaya add window and button

--- Template_4337/MainWindow.xaml.cs
+++ Template_4337/MainWindow.xaml.cs
@@ -30,5 +30,11 @@ namespace Template_4337
             var window = new Khuzyakaev_4337();
             window.Show();
         }
+
+        private void Sosnovskaya_4337_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Sosnovskaya_4337();
+            window.Show();
+        }
     }
 }
