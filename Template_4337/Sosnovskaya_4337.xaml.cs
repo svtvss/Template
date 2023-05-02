40b8a24acc7c59317ac5575c4341c9b6587ef8c3
tanya <tanyasosnovskaya145@gmail.com>
2023-05-02 12:06:13 +0300

    Import Json file and Export Word

--- Template_4337/Sosnovskaya_4337.xaml.cs
+++ Template_4337/Sosnovskaya_4337.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +12,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Microsoft.Win32;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Text.Json;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
 
 namespace Template_4337
 {
@@ -59,31 +62,32 @@ namespace Template_4337
             ObjWorkExcel.Quit();
             GC.Collect();
 
-            using (Entities entities = new Entities())
+            using (ISRPO_LR2Entities db = new ISRPO_LR2Entities())
             {
                 for (int i = 1; i < _rows; i++)
                 {
-                    entities.services.Add(new services() 
+                    db.Services.Add(new Services() 
                     { 
-                        ID = int.Parse(list[i, 0]), 
-                        Name_service = list[i, 1], 
-                        Type_service = list[i, 2], 
-                        Code_service = list[i, 3], 
-                        Price = int.Parse(list[i, 4]) 
+                        IdServices = int.Parse(list[i, 0]),
+                        NameServices = list[i, 1], 
+                        TypeOfService = list[i, 2], 
+                        CodeService = list[i, 3], 
+                        Cost = int.Parse(list[i, 4]) 
                     });
+                    MessageBox.Show($"{list[i, 1]}");
                 }
                 MessageBox.Show("Успешно!");
-                entities.SaveChanges();
+                db.SaveChanges();
             }
         }
 
         private void BnExport_Click(object sender, RoutedEventArgs e)
         {
-            List<services> service;
+            List<Services> service;
 
-            using (Entities entities = new Entities())
+            using (ISRPO_LR2Entities db = new ISRPO_LR2Entities())
             {
-                service = entities.services.ToList();
+                service = db.Services.ToList();
             }
 
             List<string[]> TypeServices = new List<string[]>() { //for sheets name
@@ -115,13 +119,13 @@ namespace Template_4337
 
                 startRowIndex++;
 
-                foreach (services services in service.OrderBy(a => a.Price))
+                foreach (Services services in service.OrderBy(a => a.Cost))
                 {
-                    if (services.Type_service == TypeServices[i][0])
+                    if (services.TypeOfService == TypeServices[i][0])
                     {
-                        worksheet.Cells[1][startRowIndex] = services.ID;
-                        worksheet.Cells[2][startRowIndex] = services.Name_service;
-                        worksheet.Cells[3][startRowIndex] = services.Price;
+                        worksheet.Cells[1][startRowIndex] = services.IdServices;
+                        worksheet.Cells[2][startRowIndex] = services.NameServices;
+                        worksheet.Cells[3][startRowIndex] = services.Cost;
                         startRowIndex++;
                     }
                 }
@@ -134,6 +138,119 @@ namespace Template_4337
                 rangeBorders.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideHorizontal].LineStyle =
                 rangeBorders.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideVertical].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
                 worksheet.Columns.AutoFit();
+            }
+            app.Visible = true;
+        }
+
+        class ServicesJSON
+        {
+            public int IdServices { get; set; }
+            public string NameServices { get; set; }
+            public string TypeOfService { get; set; }
+            public string CodeService { get; set; }
+            public int Cost { get; set; }
+        }
+
+        private void BnImportJSON_Click(object sender, RoutedEventArgs e)
+        {
+            string json = File.ReadAllText(@"C:\Users\tanya\OneDrive\Рабочий стол\Импорт\1.json");
+            var service = JsonSerializer.Deserialize<List<ServicesJSON>>(json);
+            using (ISRPO_LR2Entities entities = new ISRPO_LR2Entities())
+            {
+                foreach (ServicesJSON serviceJSON in service)
+                {
+                    try
+                    {
+                        entities.Services.Add(new Services()
+                        {
+                            IdServices = serviceJSON.IdServices,
+                            NameServices = serviceJSON.NameServices,
+                            TypeOfService = serviceJSON.TypeOfService,
+                            CodeService = serviceJSON.CodeService,
+                            Cost = serviceJSON.Cost
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                MessageBox.Show("Успешно!");
+                entities.SaveChanges();
+            }
+        }
+
+        private void BnExportWord_Click(object sender, RoutedEventArgs e)
+        {
+            List<Services> servece;
+
+            using (ISRPO_LR2Entities db = new ISRPO_LR2Entities())
+            {
+                servece = db.Services.ToList();
+            }
+
+            var app = new Microsoft.Office.Interop.Word.Application();
+            Microsoft.Office.Interop.Word.Document document = app.Documents.Add();
+
+            for (int i = 0; i < _sheetsCount; i++)
+            {
+                Microsoft.Office.Interop.Word.Paragraph paragraph = document.Paragraphs.Add();
+                Microsoft.Office.Interop.Word.Range range = paragraph.Range;
+
+                List<string[]> ServecCategories = new List<string[]>() { //for sheets name
+                    new string[]{ "Прокат" },
+                    new string[]{ "Обучение" },
+                    new string[]{ "Подъем" },
+                };
+
+                var data = i == 0 ? servece.Where(o => o.TypeOfService == "Прокат")
+                        : i == 1 ? servece.Where(o => o.TypeOfService == "Обучение")
+                        : i == 2 ? servece.Where(o => o.TypeOfService == "Подъем") : servece; //sort for task
+                List<Services> currentSer = data.ToList();
+                int countStaffsInCategory = currentSer.Count();
+
+                Microsoft.Office.Interop.Word.Paragraph tableParagraph = document.Paragraphs.Add();
+                Microsoft.Office.Interop.Word.Range tableRange = tableParagraph.Range;
+                Microsoft.Office.Interop.Word.Table serTable = document.Tables.Add(tableRange, countStaffsInCategory + 1, 3);
+                serTable.Borders.InsideLineStyle =
+                serTable.Borders.OutsideLineStyle =
+                Microsoft.Office.Interop.Word.WdLineStyle.wdLineStyleSingle;
+                serTable.Range.Cells.VerticalAlignment =
+                Microsoft.Office.Interop.Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+
+                range.Text = Convert.ToString($"Категория - {ServecCategories[i][0]}");
+                range.InsertParagraphAfter();
+
+                Microsoft.Office.Interop.Word.Range cellRange = serTable.Cell(1, 1).Range;
+                cellRange.Text = "ID";
+                cellRange = serTable.Cell(1, 2).Range;
+                cellRange.Text = "Название услуги";
+                cellRange = serTable.Cell(1, 3).Range;
+                cellRange.Text = "Стоимость";
+                serTable.Rows[1].Range.Bold = 1;
+                serTable.Rows[1].Range.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+
+                int j = 1;
+                foreach (var currentServe in currentSer.OrderBy(a => a.Cost))
+                {
+                    cellRange = serTable.Cell(j + 1, 1).Range;
+                    cellRange.Text = $"{currentServe.IdServices}";
+                    cellRange.ParagraphFormat.Alignment =
+                    Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    cellRange = serTable.Cell(j + 1, 2).Range;
+                    cellRange.Text = currentServe.NameServices;
+                    cellRange.ParagraphFormat.Alignment =
+                    Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    cellRange = serTable.Cell(j + 1, 3).Range;
+                    cellRange.Text = currentServe.Cost.ToString();
+                    cellRange.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    j++;
+                }
+
+                if (i > 0)
+                {
+                    range.InsertBreak(Microsoft.Office.Interop.Word.WdBreakType.wdPageBreak);
+                }
             }
             app.Visible = true;
         }
